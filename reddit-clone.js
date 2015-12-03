27d3f8270d0635d0dec35c783167aa0db20aa13c
davidyoakum <dyoakum@gmail.com>
2015-12-02 19:16:14 -0700

    search works

--- reddit-clone.js
+++ reddit-clone.js
@@ -1,6 +1,10 @@
 angular.module("theRedditClone", ['angularMoment'])
   .controller("all", function($scope) {
 
+  $scope.sortType = 'date';
+  $scope.sortReverse = false;
+  $scope.searchPosts = '';
+
   $scope.posts = [];
   // $scope.post = {};
 
@@ -31,5 +35,11 @@ angular.module("theRedditClone", ['angularMoment'])
     $scope.thisPostComments = this.post.comments;
     $scope.showCommentSection = !$scope.showCommentSection;
   }
+
+  // A user can click to view existing comments on a specific post
+  // The number of comments is correctly pluralized
+  // A user can search through posts
+  // Animate posts as they are added and removed from the search results.
+  // Style the app. It should look better than my implementation.
 
 })
