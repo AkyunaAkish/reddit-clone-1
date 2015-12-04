4e76b81cc45ef850ca672f7afe564748281bc3f9
davidyoakum <dyoakum@gmail.com>
2015-12-04 11:02:16 -0700

    cleaned out non reddit clone stuff

--- app.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module("firstApp", []);
-app.controller("MyFirstController", function($scope){
-  // $scope.name = "Severus Snape";
-  $scope.names = ["Harry", "Ron", "Hermione", "Sirius", "Hedwig", "Tonks"];
-  $scope.symbols = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
-  $scope.nums = [1,1,2,5,6,9,9,9]
-})
